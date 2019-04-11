2b5285c0b4b7b50606628125265ce5d37bd9fb2d
German Gomez <ggome002@fiu.edu>
2019-04-11 17:37:52 -0400

    .bash_profile alias basic

---
+++ bash_profile/alias.sh
@@ -0,0 +1,7 @@
+alias start="systemctl start"
+alias status="systemctl status"
+alias stop="systemctl stop"
+alias restart="systemctl restart"
+
+alias udp="netstat -ulnp"
+alias tcp="netstat -tlnp"
